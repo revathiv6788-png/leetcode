14679403a1d3047e85bc98b457df1d0974d2832e
reva@512 <revathiv6788@gmail.com>
2026-03-17 09:54:24 +0530

    Time: 2 ms (95.54%) | Memory: 45.8 MB (47.41%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,9 +2,7 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-
         backtrack(candidates, target, 0, current, result);
-
         return result;
     }
     public void backtrack(int[] candidates, int target, int start,
@@ -16,9 +14,17 @@ class Solution {
         if(target < 0) {
             return;
         }
+        // for(int i = start; i < candidates.length; i++) {
+        //     current.add(candidates[i]);
+        //     backtrack(candidates, target - candidates[i], i, current, result);//i ethukku kudukkuromna same value vaye again reuse panna 
+        //     current.remove(current.size() - 1); // backtrack
+        // }
+
         for(int i = start; i < candidates.length; i++) {
             current.add(candidates[i]);
-            backtrack(candidates, target - candidates[i], i, current, result);
+            target=target-candidates[i];
+            backtrack(candidates, target , i, current, result);//i ethukku kudukkuromna same value vaye 
+            target=target+candidates[i];
             current.remove(current.size() - 1); // backtrack
         }
     }
