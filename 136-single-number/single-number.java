76bfe9dfbc72f76503d764d341493e76082afbeb
reva@512 <revathiv6788@gmail.com>
2026-03-22 12:14:00 +0530

    Time: 14 ms (14.53%) | Memory: 46.8 MB (78.30%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,10 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result=0;//bit manibulation without using extra space
-        for(int i:nums)
-        {
-            result=result^i;
+        // int result=0;//bit manibulation without using extra space
+        // for(int i:nums)
+        // {
+        //     result=result^i;
+        // }
+        // return result;
+
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return result;
+
+        // Find element with count 1
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+
+        return -1; 
     }
 }
