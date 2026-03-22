15df492a401bdcc5d91c5e0e63b3136710fc6254
reva@512 <revathiv6788@gmail.com>
2026-03-22 13:05:21 +0530

    Time: 2 ms (70.68%) | Memory: 55.9 MB (27.99%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,18 +1,35 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();//but ithu space o(n)
-        for(int num:nums)
-        {
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        for(int key:map.keySet())
-        {
-            if(map.get(key)>n/2)
-            {
-                return key;
+        // int n=nums.length;
+        // HashMap<Integer,Integer> map=new HashMap<>();//but ithu space o(n)
+        // for(int num:nums)
+        // {
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
+        // for(int key:map.keySet())
+        // {
+        //     if(map.get(key)>n/2)
+        //     {
+        //         return key;
+        //     }
+        // }
+        // return -1;
+
+
+
+        //o(1) space complexcity boyer moore algorithm
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {//first time 2 if um work agum
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
-        return -1;
+        return candidate;
     }
 }
