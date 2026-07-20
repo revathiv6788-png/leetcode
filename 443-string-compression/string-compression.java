e1171dd55e8e69c7fee89b3cc3854a62e4c63038
Mariammal <revathiv6788@gmail.com>
2026-07-20 20:56:18 +0530

    Time: 1 ms (99.89%) | Memory: 45.6 MB (32.79%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -24,35 +24,24 @@
 
 class Solution {
     public int compress(char[] chars) {
-
         int n = chars.length;
         int i = 0;
         int write = 0;
-
         while (i < n) {
-
             char current = chars[i];
             int count = 0;
-
-            // Count consecutive characters
             while (i < n && chars[i] == current) {
                 count++;
                 i++;
             }
-
-            // Write the character
             chars[write++] = current;
-
-            // Write the count if greater than 1
             if (count > 1) {
                 String s = String.valueOf(count);
-
                 for (char c : s.toCharArray()) {
                     chars[write++] = c;
                 }
             }
         }
-
         return write;
     }
 }
